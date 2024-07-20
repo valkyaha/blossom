24d3fe88e7b1ded4f5cfb5a215e535ef83102421
Valkyaha <darkvalhalla@protonmail.ch>
2024-07-21 00:26:26 +0200

    Refactored and fixed App CartTemplate created SQLController refactored

--- blossom/SQLController.gd
+++ blossom/SQLController.gd
@@ -1,101 +1,96 @@
 extends Control
 
 var database: SQLite
-var amountOfUsers = 5;
+var amountOfUsers = 5
 
-@onready var random_numer = $RandomNumer
+@onready var card_placeholder = $CardPlaceholder
+@onready var random_number = $RandomNumber
 @onready var control_card_id = $ControlCardId
+@onready var ability = $Ability
+@onready var type = $Type
 
-# Called when the node enters the scene tree for the first time.
+var card_image = null
+var card_name = ""
+var ability_name = ""
+var ability_description = ""
+var card_damage = ""
+var card_hp = ""
+var card_type = ""
+
 func _ready():
 	database = SQLite.new()
-	database.path="res://data/data.db"
-	database.open_db()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	database.path = "res://data/data.db"
+	if not database.open_db():
+		printerr("Failed to open database.")
 	pass
-
 
 func _on_insert_button_down():
-	var data = {
-		"type" : int($CardType.text),
-		"name" : $CardName.text,
-		"damage" : int($CardDamage.text),
-		"health" : int($CardHealth.text),
-		"ability" : int($CardAbilityName.text)
-	}
-	
-	database.insert_row("cards",data)
-	print(data)
+	var data = card_placeholder.export_data()
+	data["ability"] = ability.text
+	data["type"] = type.text.to_int()
+	data.erase("id")
+	if not database.insert_row("cards", data):
+		printerr("Failed to insert row into database.")
 	pass
-
 
 func _on_update_button_down():
-	var data = {
-		"type" : int($CardType.text),
-		"name" : $CardName.text,
-		"damage" : int($CardDamage.text),
-		"health" : int($CardHealth.text),
-		"ability" : int($CardAbilityName.text)
-	}
-
-	print(data)
-	print($CardName.text)
-	database.update_rows("cards","name = '" + $CardName.text + "'",data)
-	
+	var data = card_placeholder.export_data()
+	data["ability"] = ability.text
+	data["type"] = type.text.to_int()
+	card_name = data["name"]
+	var str_card_id = str(data["id"])
+	data.erase("id")
+	if not database.update_rows("cards", "id = '" + str_card_id + "'", data):
+		printerr("Failed to update row in database.")
 	pass
-
 
 func _on_select_button_down():
-	var cards = database.select_rows("cards","id =" + $ControlCardId.text,["*"])
+	var cards = database.select_rows("cards", "id = " + control_card_id.text, ["*"])
+	if cards.size() == 0:
+		card_placeholder.data(0, null, "", "", "", "", "", "")
+		printerr("No cards found with the given ID.")
+		return
 	
-	if (cards.size() == 0):
-		$CardName.text = ""
-		$CardType.text = ""
-		$CardDamage.text = ""
-		$CardHealth.text = ""
-		$CardAbilityName.text = ""
-		$CardAbilityDescription.text = ""
-		$CardImage.texture = null
+	var card = cards[0]
+	card_name = card["name"]
+	var card_id = card["id"]
 	
-	for i in cards: 
-		$CardName.text = i["name"]
-		var type = database.select_rows("type","id =" + str(i["type"]),["*"])
-		$CardType.text = type[0]["type"]
-		$CardDamage.text = str(i["damage"])
-		$CardHealth.text = str(i["health"])
-		
-		var ability = database.select_rows("ability","id ="+ str(i["ability"]),["*"])
-		$CardAbilityName.text = ability[0]["ability_name"]
-		$CardAbilityDescription.text = ability[0]["effect"]
-		
-		if(i["image"] != null):		
-			var image = Image.new()
-			image.load_png_from_buffer(i["image"])
-			var texture = ImageTexture.create_from_image(image)
-			$CardImage.texture = texture
-		else :
-			$CardImage.texture = null
-	pass
+	var card_type_result = database.select_rows("type", "id = " + str(card["type"]), ["*"])
+	if card_type_result.size() > 0:
+		card_type = card_type_result[0]["type"]
+	
+	card_damage = str(card["damage"])
+	card_hp = str(card["health"])
 
+	var ability_result = database.select_rows("ability", "id = " + str(card["ability"]), ["*"])
+	if ability_result.size() > 0:
+		ability_name = ability_result[0]["ability_name"]
+		ability_description = ability_result[0]["effect"]
+
+	if card["image"] != null:
+		var image = Image.new()
+		image.load_png_from_buffer(card["image"])
+		var texture = ImageTexture.create_from_image(image)
+		card_image = texture
+	else:
+		card_image = null
+	
+	card_placeholder.data(card_id, card_image, card_name, ability_name, ability_description, card_damage, card_hp, card_type)
+	pass
 
 func _on_delete_button_down():
+	var card_id = control_card_id.text
+	if not database.delete_rows("cards", "id = " + card_id):
+		printerr("Failed to delete row from database.")
 	pass
-
 
 func _on_query_button_down():
-	
-	var image := preload("res://resources/CardTemplate/Cards_color1/Civilian_card_back/details/Background.png")
+	var image = preload("res://resources/CardTemplate/Cards_color1/Civilian_card_back/details/Background.png")
 	var pba = image.get_image().save_png_to_buffer()
-	database.update_rows("cards","id = 1",{"image": pba})
-
+	if not database.update_rows("cards", "id = 1", {"image": pba}):
+		printerr("Failed to update row in database.")
 	pass
 
-
 func _on_roulette_button_down():
-	random_numer.text = str(randi() % amountOfUsers)
-	
+	$RandomNumer.text = str(randi() % amountOfUsers)
 	pass
--- blossom/scenes/scripts/CardTemplateLogic.gd
+++ blossom/scenes/scripts/CardTemplateLogic.gd
@@ -1,11 +1,46 @@
 extends Node2D
 
+var card_id = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func data(id, image, card_name, ability_name, ability_description, card_damage, card_hp, card_type):
+	$CardImage.texture = image
+	$CardName.text = card_name
+	$AbilityName.text = ability_name
+	$AbilityDescription.text = ability_description
+	$CardDamage.text = card_damage
+	$CardHP.text = card_hp
+	$CardType.text = card_type
+	card_id = id
+
+
+func get_input_data():
+	var data = {
+		"id" : card_id,
+		"type": int($CardType.text),
+		"name": $CardName.text,
+		"damage": int($CardDamage.text),
+		"health": int($CardHP.text),
+	}
+	return data
+
+func export_data():
+	var image_data = null
+	var pba = null
+	if $CardImage.texture != null:
+		pba = $CardImage.texture.get_image().save_png_to_buffer()
+
+	var data = {
+		"id" : card_id,
+		"image": pba,
+		"name": $CardName.text,
+		"damage": int($CardDamage.text),
+		"health": int($CardHP.text),
+		"type": int($CardType.text)
+	}
+	return data
