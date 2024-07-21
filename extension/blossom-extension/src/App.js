ca19bf0a4422644c3753c00d667ebfd99da5f2a6
Valkyaha <darkvalhalla@protonmail.ch>
2024-07-21 21:05:53 +0200

    Refactored card to allow inputs

--- extension/blossom-extension/src/App.js
+++ extension/blossom-extension/src/App.js
@@ -5,26 +5,22 @@ import {CardTemplateComponent} from "./components/CardTemplateComponent";
 const cardData = {
     dmg: '10',
     hp: '20',
-    image: 'path_to_image.png',
-    name: 'Hero Name',
-    abilityName: 'Special Ability',
-    abilityDescription: 'This is a description of the special ability.',
+    image: 'https://preview.redd.it/deer-godskin-shikanoko-the-scarlet-brainrot-v0-dkvl85w24pad1.jpeg?width=1920&format=pjpg&auto=webp&s=913c408a1333fff6aa3f00f91792a0cf056d0636',
+    name: 'Shikanoko Noko',
+    abilityName: 'Shikanoko',
+    abilityDescription: 'Shikanoko noko koshi tan tan shikanoko noko koshi tan tan shikanoko noko koshi tan tan shikanoko noko koshi tan tan',
     tp: '5'
+};
+
+const handleSubmit = (data) => {
+    console.log('Form data:', data);
 };
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    PITO CORTO IS ALIVE adsadasdas
-                </a>
-                <CardTemplateComponent data={cardData}/>
+                <CardTemplateComponent initialData={cardData} onSubmit={handleSubmit}/>
             </header>
         </div>
     );
--- extension/blossom-extension/src/components/CardTemplateComponent.jsx
+++ extension/blossom-extension/src/components/CardTemplateComponent.jsx
@@ -1,22 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./CardTemplateComponent.css";
 
-export const CardTemplateComponent = ({ data }) => {
+export const CardTemplateComponent = ({ initialData, onSubmit }) => {
+    const [data, setData] = useState(initialData);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData({
+            ...data,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(data);
+    };
+
     return (
-        <div id="cardBody">
+        <form id="cardBody" onSubmit={handleSubmit}>
             <div id="cardHeader">
-                <div id="dmg">{data.dmg}</div>
-                <div id="hp">{data.hp}</div>
+                <input
+                    type="text"
+                    name="dmg"
+                    value={data.dmg}
+                    onChange={handleChange}
+                    className="cardInput"
+                    placeholder="DMG"
+                />
+                <input
+                    type="text"
+                    name="hp"
+                    value={data.hp}
+                    onChange={handleChange}
+                    className="cardInput"
+                    placeholder="HP"
+                />
             </div>
             <div id="cardImageContainer">
                 <img id="cardImage" src={data.image} alt="Card Image" />
             </div>
+            <input
+                type="text"
+                name="name"
+                value={data.name}
+                onChange={handleChange}
+                id="name"
+                className="cardInput"
+                placeholder="Name"
+            />
             <div id="cardFooter">
-                <div id="name">{data.name}</div>
-                <div id="abilityName">{data.abilityName}</div>
-                <div id="abilityDescription">{data.abilityDescription}</div>
-                <div id="tp">{data.tp}</div>
+                <input
+                    type="text"
+                    name="abilityName"
+                    value={data.abilityName}
+                    onChange={handleChange}
+                    className="cardInput"
+                    placeholder="Ability Name"
+                />
+                <input
+                    type="text"
+                    name="abilityDescription"
+                    value={data.abilityDescription}
+                    onChange={handleChange}
+                    className="cardInput"
+                    placeholder="Ability Description"
+                />
+                <input
+                    type="text"
+                    name="tp"
+                    value={data.tp}
+                    onChange={handleChange}
+                    className="cardInput"
+                    placeholder="TP"
+                />
+                {/*<button type="submit" className="submitButton">Save</button>*/}
             </div>
-        </div>
+        </form>
     );
 }
